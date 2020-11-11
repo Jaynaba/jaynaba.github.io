e794661647adadcefb1d6eaaee4876be3dc99637
Jaynaba <35877365+Jaynaba@users.noreply.github.com>
2020-11-11 00:55:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let evrbutt = document.querySelector("#everyone");
+let par1 = document.querySelector("#par1");
+evrbutt.addEventListener('click', evrfunc);
+function evrfunc(){
+	 par1.textContent= "i think at one point or another, everyone realizes that they will, in fact, die and depending on who you are, you handle that at varying ranges. in regards to marshall mcluhan’s work, i think there is a reiteration of time and life and how people spend theirs. most notably, it is this motif throughout the sections of people continuously trying to do new things with old tools, and this idea of being unable to let go of one’s past. this piece was written in the 60s and while it’s interesting that a lot of its material still holds true to this day, it is also a bit terrifying. i don’t know what the meaning of life is but i really don’t think it’s trying to fulfill some path that’s already been crossed way before your time and will continue to be when you’re gone. i don’t want to look back with my only accomplishment being that i did what was expected of me, and if there’s one thing this pdf instills, it’s change. so maybe i should try and do that, at least just a little bit. maybe we all should.";	
+  }
+
+let envbutt = document.querySelector("#environment");
+let par2 = document.querySelector("#par1");
+envbutt.addEventListener('click', envfunc);
+function envfunc(){
+	 par2.textContent= "our surrounding environment is one of the most important elements to our lives and well-being. i think it is where one developments their standards for most aspects of life and has a lot to do with how we perceive the outside world. with time, one has the opportunity to truly learn of other aspects of living and different environments by way of the internet and other media including film and television. the medium of the massage, at least to me, further exemplifies the importance of changing one’s environment and being open to new ones, even those that seem foreign and unheard of. it’s about carving out your own journey regardless of nay-sayers and your upbringing influencing you to believe you weren’t smart enough, or talented enough, or good enough. none of that stuff matters, because those older and/or less brave people are just living based on what they know, what their environment taught them because they were too scared to question what else was out there. you don’t have to be. ";
+}
+
+let joybutt = document.querySelector("#joy");
+let par3 = document.querySelector("#par1");
+joybutt.addEventListener('click', joyfunc);
+function joyfunc(){
+	 par3.textContent="i think there is joy in the process of figuring out what truly brings that to you, be it your favorite old cartoon that plays at dawn or the cat that lives in the snack aisle of your favorite bodega. maybe it’s the sound of the subway tracks or the yellow sweaters. i don’t know. but i think there is something that happens within you when you fully recognize what you want in your life and what you don’t. that individuality is a beautiful thing to evolve and bloom. the sounds and colors that galvanize certain emotions and associations. it’s all stunning. and it’s all important because it intertwines with your creation, not realization. you don’t just know who you are from the jump, it’s a constant process and iteration of taking in some aspects of your environment and life, disregarding others but still learning from them. what you realize was that reaching a certain segment of your life is not what brought joy, but the messy navigation leading to it.";
+}
